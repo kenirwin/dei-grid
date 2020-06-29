8ea7099d4788117620bed4e41f2038e8b5246293
Ken Irwin <irwinkr@miamioh.edu>
2020-06-29 17:22:57 -0400

    table displays, cell assignment works

---
+++ questions.js
@@ -0,0 +1,96 @@
+let questions = [
+  {
+    question: 'Accessibility in procurement',
+    scores: [
+      [4, 2],
+      [4, 2],
+      [2, 2],
+      [2, 2],
+      [5, 3],
+      [4, 1],
+    ],
+  },
+];
+
+let text = [
+  [1, 'one'],
+  [2, 'two'],
+  [3, 'three'],
+  [4, 'four'],
+  [5, 'five'],
+];
+
+let tableGrid = '<table class="dei-grid">\n';
+tableGrid += '  <thead>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th>X</th>\n';
+tableGrid += '      <th scope="col">Very Easy</th>\n';
+tableGrid += '      <th scope="col">Moderately Easy</th>\n';
+tableGrid += '      <th scope="col">Neither Easy Nor Difficult</th>\n';
+tableGrid += '      <th scope="col">Moderately Difficult</th>\n';
+tableGrid += '      <th scope="col">Very Difficult</th>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '  </thead>\n';
+tableGrid += '  <tbody>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th scope="row">Very Important</th>\n';
+tableGrid += '      <td class="one-one"></td>\n';
+tableGrid += '      <td class="one-two"></td>\n';
+tableGrid += '      <td class="one-three"></td>\n';
+tableGrid += '      <td class="one-four"></td>\n';
+tableGrid += '      <td class="one-five"></td>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th scope="row">Moderately Important</th>\n';
+tableGrid += '      <td class="two-one"></td>\n';
+tableGrid += '      <td class="two-two"></td>\n';
+tableGrid += '      <td class="two-three"></td>\n';
+tableGrid += '      <td class="two-four"></td>\n';
+tableGrid += '      <td class="two-five"></td>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th scope="row">Middle Importance</th>\n';
+tableGrid += '      <td class="three-one"></td>\n';
+tableGrid += '      <td class="three-two"></td>\n';
+tableGrid += '      <td class="three-three"></td>\n';
+tableGrid += '      <td class="three-four"></td>\n';
+tableGrid += '      <td class="three-five"></td>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th scope="row">Less Important</th>\n';
+tableGrid += '      <td class="four-one"></td>\n';
+tableGrid += '      <td class="four-two"></td>\n';
+tableGrid += '      <td class="four-three"></td>\n';
+tableGrid += '      <td class="four-four"></td>\n';
+tableGrid += '      <td class="four-five"></td>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '    <tr>\n';
+tableGrid += '      <th scope="row">Not Important</th>\n';
+tableGrid += '      <td class="five-one"></td>\n';
+tableGrid += '      <td class="five-two"></td>\n';
+tableGrid += '      <td class="five-three"></td>\n';
+tableGrid += '      <td class="five-four"></td>\n';
+tableGrid += '      <td class="five-five"></td>\n';
+tableGrid += '    </tr>\n';
+tableGrid += '  </tbody>\n';
+tableGrid += '</table>\n';
+
+$(document).ready(function () {
+  let count = 0;
+  questions.forEach((i) => {
+    count++;
+    $('#container').append(
+      '<div class="table-set" id="table-set-' +
+        count +
+        '"><h2>' +
+        i.question +
+        '</h2>' +
+        tableGrid +
+        '</div>'
+    );
+    $('#table-set-' + count + ' table')
+      .find('.one-three')
+      .text(3)
+      .addClass('.level-' + 3);
+  });
+});
